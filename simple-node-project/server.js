5d3751d9b745b652c9088ac20fe7024924795e8c
thedarklord000 <suhelpashasp1998@gmail.com>
2024-02-29 20:09:12 +0530

    commit

--- simple-node-project/server.js
+++ simple-node-project/server.js
@@ -6,17 +6,35 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    const filePath = path.join(__dirname, 'index.html');
-
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        }
-    });
+    if (req.url === '/' && req.method === 'GET') {
+        // Serve index.html
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        });
+    } else if (req.url === '/createFile' && req.method === 'POST') {
+        // Create sample.txt file
+        const filePath = path.join(__dirname, 'sample.txt');
+        fs.writeFile(filePath, 'This is a sample file created by the server.', (err) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.end('sample.txt created successfully!');
+            }
+        });
+    } else {
+        // Handle other routes or methods
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
 });
 
 server.listen(PORT, () => {
